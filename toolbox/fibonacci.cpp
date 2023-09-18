82149e62150606be4dcc20364f23d6fedb11bac7
AleAro21 <A01782691@tec.mx>
2023-09-18 10:41:18 -0600

    fib dymaic

--- toolbox/fibonacci.cpp
+++ toolbox/fibonacci.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <map>
+
 
 
 using namespace std;
@@ -11,21 +13,44 @@ using namespace std;
 //     return fib(n-1) + fib(n-2);
 // }
 
-int fibSuma(int n){
-    int FF[n];
-    FF[0] = 0;
-    FF[1] = 1;
-    for(int i = 2; i <= n; i++){
-        FF[i] = FF[i-1] + FF[i-2];
-    }
-    return FF[n];
+
+
+// int fibSuma(int n){
+//     int FF[n];
+//     FF[0] = 0;
+//     FF[1] = 1;
+//     for(int i = 2; i <= n; i++){
+//         FF[i] = FF[i-1] + FF[i-2];
+//     }
+//     return FF[n];
+// }
+
+
+
+map<int, int> init_map() {
+    map<int, int> temp;
+    temp[0] = 0;
+    temp[1] = 1;
+    return temp;
 }
 
-int main(){
+map<int, int> my_map = init_map();
+
+int fib(int n) {
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+    if (my_map.find(n) != my_map.end()) {
+        return my_map[n];
+    }
+    my_map[n] = fib(n-1) + fib(n-2);
+    return my_map[n];
+}
+
+int main() {
     int n;
     cout << "Número: ";
     cin >> n;
-    cout << fibSuma(n) << endl;
+    cout << fib(n) << endl;
 
     return 0;
 }
