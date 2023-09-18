ef47c67dba0ca0f9e001d752b3dc66b130e66376
AleAro21 <A01782691@tec.mx>
2023-09-18 10:49:07 -0600

    format on save

--- toolbox/bubbleSort.cpp
+++ toolbox/bubbleSort.cpp
@@ -2,20 +2,24 @@
 
 using namespace std;
 
-
-int bubbleSort(int arr[], int n){
+int bubbleSort(int arr[], int n)
+{
     int temp;
-    for(int i = 0; i < n-1; i++){
+    for (int i = 0; i < n - 1; i++)
+    {
         bool swapped = false;
-        for(int j = 0; j < n-i-1; j++){
-            if(arr[j] > arr[j+1]){
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
                 swapped = true;
-                temp = arr[j+1];
-                arr[j+1] = arr[j];
+                temp = arr[j + 1];
+                arr[j + 1] = arr[j];
                 arr[j] = temp;
             }
         }
-        if(!swapped){
+        if (!swapped)
+        {
             break;
         }
     }
--- toolbox/quickSort.cpp
+++ toolbox/quickSort.cpp
@@ -2,23 +2,24 @@
 
 using namespace std;
 
+void quickSort(int a[], int lo, int hi)
+{
+   if (hi <= lo)
+      return;
+   int j = partition(a, lo, hi);
+   quickSort(a, lo, j - 1);
+   quickSort(a, j + 1, hi);
+}
 
- void quickSort (int a[], int lo, int hi)
- {
-    if (hi <= lo) return;
-    int j = partition(a, lo, hi);
-    quickSort(a, lo, j-1);
-    quickSort(a, j+1, hi);
- }
-
- int partition (int a[], int lo, int hi)
- {
-    int i = lo, j = hi+1;
-    int v = a[lo];
-    while (true){
-        if (i >= j) break;
-    }
-    v = a[j];
-    return j;
- }
-
+int partition(int a[], int lo, int hi)
+{
+   int i = lo, j = hi + 1;
+   int v = a[lo];
+   while (true)
+   {
+      if (i >= j)
+         break;
+   }
+   v = a[j];
+   return j;
+}
